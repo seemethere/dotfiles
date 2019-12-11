b3c737c543a4d75b7854bfcdbe5f6bfd41bc84ee
Eli Uriegas <eliuriegas@fb.com>
2019-12-10 19:30:08 -0800

    hammerspoon: Add initial config
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

---
+++ hammerspoon/init.lua
@@ -0,0 +1,15 @@
+hs.window.animationDuration = 0
+units = {
+  right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+  left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+  top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+  bottom50      = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+}
+
+mash = { 'option', 'ctrl', 'cmd' }
+hs.hotkey.bind(mash, 'l', function() hs.window.focusedWindow():move(units.right50,    nil, true) end)
+hs.hotkey.bind(mash, 'h', function() hs.window.focusedWindow():move(units.left50,     nil, true) end)
+hs.hotkey.bind(mash, 'k', function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
+hs.hotkey.bind(mash, 'j', function() hs.window.focusedWindow():move(units.bottom50,   nil, true) end)
+hs.hotkey.bind(mash, 'm', function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
