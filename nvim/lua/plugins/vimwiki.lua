f5ea86948700337405d19b9ccf01aeaa34d91e2b
Eli Uriegas <seemethere101@gmail.com>
2023-10-07 18:26:36 -0700

    nvim: Add back rooter and vimwiki
    Signed-off-by: Eli Uriegas <seemethere101@gmail.com>

---
+++ nvim/lua/plugins/vimwiki.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "vimwiki/vimwiki",
+    init = function() --replace 'config' with 'init'
+      vim.g.vimwiki_list = { { path = "~/vimwiki", syntax = "markdown", ext = ".md" } }
+    end,
+  },
+}
