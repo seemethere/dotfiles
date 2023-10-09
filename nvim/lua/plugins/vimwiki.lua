c10c61ae8be1fead2f165e4fae3c4b06e427cb1e
Eli Uriegas <seemethere101@gmail.com>
2023-10-09 09:30:10 -0700

    nvim: Change vimwiki extensions to .wiki
    Signed-off-by: Eli Uriegas <seemethere101@gmail.com>

--- nvim/lua/plugins/vimwiki.lua
+++ nvim/lua/plugins/vimwiki.lua
@@ -2,7 +2,7 @@ return {
   {
     "vimwiki/vimwiki",
     init = function() --replace 'config' with 'init'
-      vim.g.vimwiki_list = { { path = "~/vimwiki", syntax = "markdown", ext = ".md" } }
+      vim.g.vimwiki_list = { { path = "~/vimwiki", syntax = "markdown", ext = ".wiki" } }
     end,
   },
 }
