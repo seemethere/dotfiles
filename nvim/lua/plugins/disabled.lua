85d7cc9b3356ba5a63b130553e507d3d6006091d
Eli Uriegas <eliuriegas@meta.com>
2024-03-06 13:20:54 -0800

    nvim: Separate out nvim disables to separate file
    Signed-off-by: Eli Uriegas <eliuriegas@meta.com>

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  -- disable mini.surround from the default LazyVim distribution
+  { "echasnovski/mini.surround", enabled = false },
+  -- directly conflicts with typical nvim-surroundkeymaps
+  { "folke/flash.nvim", enabled = false },
+  -- causes a ton of slow downs for me on macOS Sonoma
+  -- { "echasnovski/mini.indentscope", enabled = false },
+}
--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,8 +1,4 @@
 return {
-  -- disable mini.surround from the default LazyVim distribution
-  { "echasnovski/mini.surround", enabled = false },
-  -- directly conflicts with typical nvim-surroundkeymaps
-  { "folke/flash.nvim", enabled = false },
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
