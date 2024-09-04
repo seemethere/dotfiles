53189b84d042647381beab7178f4c7ee98276e85
Eli Uriegas <seemethere101@gmail.com>
2024-09-03 22:43:18 -0700

    nvim: Switch vimwiki to use markdown
    Signed-off-by: Eli Uriegas <seemethere101@gmail.com>

--- nvim/lua/plugins/vimwiki.lua
+++ nvim/lua/plugins/vimwiki.lua
@@ -2,7 +2,7 @@ return {
   {
     "vimwiki/vimwiki",
     init = function() --replace 'config' with 'init'
-      vim.g.vimwiki_list = { { path = "~/vimwiki", syntax = "markdown", ext = ".wiki" } }
+      vim.g.vimwiki_list = { { path = "~/vimwiki" } }
     end,
   },
 }
